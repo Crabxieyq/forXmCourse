ade83fb9e8784f0f8c9bb6572d972d23f24e8fca
谢燕青 <yanqing.xie@kingstarfintech.com>
2023-10-07 17:26:57 +0800

    全局变量/函数

--- src/Layout/Header/index.vue
+++ src/Layout/Header/index.vue
@@ -4,13 +4,14 @@
  * @Author: yanqing.Xie
  * @Date: 2023-10-02 19:25:44
  * @LastEditors: yanqing.Xie
- * @LastEditTime: 2023-10-02 19:47:00
+ * @LastEditTime: 2023-10-07 17:25:23
 -->
 <template>
     <div class="xi-header">
         Header
         {{ fromBro }}
         <button @click="toMenu">通过引入的mitt给兄弟组件传值</button>
+        全局变量{{ $env }}
     </div>
 </template>
 
@@ -22,12 +23,22 @@ import Bus from '../../bus'
 const instance = getCurrentInstance()
 let toMenuMethods = ref<string>('header通过引入的mitt Bus给Menu传值')
 const toMenu = () => {
-    instance?.proxy?.$Bus.emit('toMenu',toMenuMethods)
+    instance?.proxy?.$Bus.emit('toMenu',toMenuMethods)//这里的$bus相当于全局变量
+    
+    
 }
+console.log(instance?.proxy?.$env);//读取$env全局变量
 let fromBro = ref<string>('')
 Bus.on('toHeader',(toBro:string) => {
     fromBro.value = toBro
 })
+
+
+declare module 'vue' {
+    export interface ComponentCustomProperties {
+        $env: string
+    }
+}
 </script>
 <style scoped lang="scss">
 @include b(header){
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@
  * @Author: yanqing.Xie
  * @Date: 2023-10-02 14:26:03
  * @LastEditors: yanqing.Xie
- * @LastEditTime: 2023-10-04 18:42:07
+ * @LastEditTime: 2023-10-07 17:21:33
  */
 import { createApp } from 'vue'
 // import './style.css'
@@ -14,6 +14,10 @@ import slotCom from './components/DynamicComponent/Dialog/index.vue'
 import mitt from 'mitt'
 const Mit = mitt()
 const app = createApp(App)
+// 定义全局变量 使用时直接用$env 即可
+app.config.globalProperties.$env = 'dev'
+
+
 declare module 'vue'{
     export interface ComponentCustomProperties{
         $Bus:typeof Mit
